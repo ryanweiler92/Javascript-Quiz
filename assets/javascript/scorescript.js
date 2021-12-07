72fe5333fcb330822f7e9a273bd0c308855ee498
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-07 10:26:22 -0500

    load high score functions work on load

--- assets/javascript/scorescript.js
+++ assets/javascript/scorescript.js
@@ -1,28 +1,28 @@
-var scoreLoadButton = document.getElementById('score-load-button');
+//var scoreLoadButton = document.getElementById('score-load-button');
 
-scoreLoadButton.addEventListener('click', postHighScores);
+//scoreLoadButton.addEventListener('click', postHighScores);
 
 
-var postHighScores = function(e) {
-    
+function postHighScores(e) {
+
     console.log('test');
-    // var retrieveScore = localStorage.getItem('Score');
-    // var retrieveInitial = localStorage.getItem("initials");
+    var retrieveScore = localStorage.getItem('Score');
+    var retrieveInitial = localStorage.getItem("initials");
 
-    // var tableVar = document.getElementById("high-score-table");
+    var tableVar = document.getElementById("high-score-table");
 
-    // var tableRow = document.createElement('tr');
-    // tableVar.appendChild(tableRow);
+    var tableRow = document.createElement('tr');
+    tableVar.appendChild(tableRow);
 
-    // var tableDataInitial = document.createElement('td');
-    // tableRow.appendChild(tableDataInitial);
-    // tableDataInitial.textContent = retrieveInitial;
+    var tableDataInitial = document.createElement('td');
+    tableRow.appendChild(tableDataInitial);
+    tableDataInitial.textContent = retrieveInitial;
 
-    // var tableDataScore = document.createElement('td')
-    // tableRow.appendChild(tableDataScore);
-    // tableDataScore.textContent = retrieveScore;
+    var tableDataScore = document.createElement('td')
+    tableRow.appendChild(tableDataScore);
+    tableDataScore.textContent = retrieveScore;
 
-    // e.preventDefault();
+    
 }
 
 
